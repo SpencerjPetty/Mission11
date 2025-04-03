c73fb8be90b06a03e41d4666b925504da7b4f5d1
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2025-04-02 19:15:46 -0600

    yay

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61221284660d09cbc536cf942d3aae1551c41c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98065f50920a2097c82ad02f41a54db65edb8b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
